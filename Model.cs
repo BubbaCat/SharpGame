66b30b22aa005e6d52f8e99192ef78dc8ee68af7
Яна Лапердина <yana.laperdina@gmail.com>
2021-06-22 17:12:37 +0500

    Adding Model
    adding model for player terrain wall dor and proffesor

---
+++ Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public class Player : ICreature
+    {
+
+    }
+
+    public class Terrain : ICreature
+    {
+
+    }
+
+
+    public class Wall : ICreature
+    {
+        
+       
+    }
+
+
+    public class Door : ICreature
+    {
+ 
+    }
+
+    public class Professor : ICreature
+    {
+
+        }
+
+
+    }
+}
