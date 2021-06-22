12127726aee3398f99984d56dd9496be10f1c4cc
Evgeniy <evgeniy.sapov@gmail.com>
2021-06-22 16:59:30 +0500

    Added class Game for creating map and saving game states

---
+++ Model_Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace Game
+{
+    public static class Game
+    {
+        private const string map = @"
+PTTGTTTTTTTTTTTTTTTTTTTT
+SSSSSSSTTTTTSSSSSSSTTTTT
+TTTMTTSTTTTTSTTMTTSTTTTT
+TTTTTTSTTTTTSTTTTTSTTTTT
+TTTTTTSTTTTTSTTTTTSTTTTT
+TTTMTTGTTTTTGTTMTTGTTTTT
+SSSGSSSTTTTTSSSGSSSTTTTT
+TTTTTTSTTTTTSTTTTTSTTTTT
+TTTTTTGTTTTTGTTTTTGTTTTT
+GTTTTMSTTTTTSTTTTMSTTTTT";
+
+        public static ICreature[,] Map;
+        public static int Scores;
+        public static bool IsOver;
+        public static Keys KeyPressed;
+        public static int MapWidth => Map.GetLength(0);
+        public static int MapHeight => Map.GetLength(1);
+        public static void CreateMap()=> Map = CreatureMapCreator.CreateMap(map);
+    }
+}
